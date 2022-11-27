34cf02f2a1c81c6cd69a71032f6978c824bf6b59
Golyb4ik <only_game2@mail.ru>
2022-11-27 18:01:04 +0300

    1.3

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        int age=24;
-        if (age>=7&&age<18) {
-            System.out.println("Ребенок ходит в школу");
+        int passenger=59;
+        if (passenger>=103) {
+            System.out.println("Вагон полностью забит");
         }
-        if (age>=18&&age<24) {
-            System.out.println("Человек закончил школу и может отправляться в университет");
+        if (passenger<60) {
+            System.out.println("Есть сидячее место");
         }
-        if (age>=24) {
-            System.out.println("Человк окончил университет и ему пора искать первую работу");
+        if (passenger>60&&passenger<=102) {
+            System.out.println("Есть стоячее место");
         }
         }
 }

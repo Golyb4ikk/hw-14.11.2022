8f6f65db9a7f3fa12f2a47d1602b309c7838ff06
Golyb4ik <only_game2@mail.ru>
2022-11-27 17:32:58 +0300

    1.2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        int age=19;
-        if (age>=7) {
+        int age=24;
+        if (age>=7&&age<18) {
             System.out.println("Ребенок ходит в школу");
         }
-        if (age>=18) {
+        if (age>=18&&age<24) {
             System.out.println("Человек закончил школу и может отправляться в университет");
         }
         if (age>=24) {

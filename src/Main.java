1c40e93f19981f01d84f31fc30bfe5efbc67aa4d
Golyb4ik <only_game2@mail.ru>
2022-11-27 13:29:29 +0300

    1.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+        int age=17;
+        if (age>=18) {
+            System.out.println("Поздравляю с совершеннолетием!");
+        }
+        if (age<18) {
+            System.out.println("Возраст совершеннолетия еще не достигнут!");
+        }
+        }
 }

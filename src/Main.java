e62aa6c4302526e3c2636adc80bca8cd411a3297
Golyb4ik <only_game2@mail.ru>
2022-11-27 18:19:26 +0300

    1.5

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        int age=17;
-        if (age>=18) {
-            System.out.println("Поздравляю с совершеннолетием!");
-        }else {
-            System.out.println("Возраст совершеннолетия еще не достигнут!");
+        int age=24;
+        if (age>=7&&age<18) {
+            System.out.println("Ребенок ходит в школу");
+        }
+        else if (age>=18&&age<24) {
+            System.out.println("Человек закончил школу и может отправляться в университет");
+        }
+        else {
+            System.out.println("Человк окончил университет и ему пора искать первую работу");
         }
     }
-    }
+}
